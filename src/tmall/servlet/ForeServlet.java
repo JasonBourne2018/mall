b667a95fc006aafc0e32fff1851f1b7e3b1ee0e2
mayunpu <1508366362@qq.com>
2020-08-25 18:59:21 +0800

    t

--- src/tmall/servlet/ForeServlet.java
+++ src/tmall/servlet/ForeServlet.java
@@ -1,12 +1,18 @@
 package tmall.servlet;
 
-import tmall.bean.Category;
+import org.springframework.web.util.HtmlUtils;
+import tmall.bean.*;
+import tmall.comparator.*;
 import tmall.dao.CategoryDAO;
 import tmall.dao.ProductDAO;
+import tmall.dao.ProductImageDAO;
 import tmall.util.Page;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -19,5 +25,158 @@ public class ForeServlet extends BaseForeServlet{
         new ProductDAO().fillByRow(cs);
         request.setAttribute("cs",cs);
         return "home.jsp";
+    }
+
+    public String register(HttpServletRequest request, HttpServletResponse response, Page page) {
+        String name = request.getParameter("name");
+        String password = request.getParameter("password");
+        name = HtmlUtils.htmlEscape(name);
+        boolean exist = userDAO.isExist(name);
+
+        if (exist) {
+            request.setAttribute("msg", "用户名已经被使用，不能使用");
+            return "register.jsp";
+        }
+
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        userDAO.add(user);
+        return "@registerSuccess.jsp";
+    }
+
+    public String login(HttpServletRequest request, HttpServletResponse response, Page page) {
+        String name = request.getParameter("name");
+        name = HtmlUtils.htmlEscape(name);
+        String password = request.getParameter("password");
+
+        User user = userDAO.get(name, password);
+        if (null==user) {
+            request.setAttribute("msg", "账号密码错误");
+            return "login.jsp";
+        }
+
+        request.getSession().setAttribute("user", user);
+        return "@forehome";
+    }
+
+    public String logout(HttpServletRequest request, HttpServletResponse response, Page page) {
+        request.getSession().removeAttribute("usre");
+        return "@forehome";
+    }
+
+    public String product(HttpServletRequest request, HttpServletResponse response, Page page) {
+        int pid = Integer.parseInt(request.getParameter("pid"));
+        Product product = productDAO.get(pid);
+
+        List<ProductImage> productSingleImages = productImageDAO.list(product, ProductImageDAO.type_single);
+        List<ProductImage> productDetailImages = productImageDAO.list(product, ProductImageDAO.type_detail);
+        product.setProductSingleImages(productSingleImages);
+        product.setProductDetailImages(productDetailImages);
+
+        List<PropertyValue> pvs = propertyValueDAO.list(product.getId());
+        List<Review> reviews = reviewDAO.list(product.getId());
+
+        productDAO.setSaleAndReviewNumber(product);
+
+        request.setAttribute("reviews", reviews);
+        request.setAttribute("p",product);
+        request.setAttribute("pvs", pvs);
+        return "product.jsp";
+    }
+
+    public String checkLogin(HttpServletRequest request, HttpServletResponse response, Page page) {
+        User user = (User) request.getSession().getAttribute("user");
+        if (null==user) {
+            return "%fail";
+        }
+        return "%success";
+    }
+
+    public String loginAjax(HttpServletRequest request, HttpServletResponse response, Page page) {
+        String name = request.getParameter("name");
+        name = HtmlUtils.htmlEscape(name);
+        String password = request.getParameter("password");
+        User user = userDAO.get(name, password);
+        if (null==user) {
+            request.setAttribute("msg", "账号密码错误");
+            return "%fail";
+        }
+        request.getSession().setAttribute("user", user);
+        return "%success";
+    }
+
+    public String Category(HttpServletRequest request, HttpServletResponse response, Page page) {
+        int cid = Integer.parseInt(request.getParameter("cid"));
+        Category c = categoryDAO.get(cid);
+        new ProductDAO().fill(c);
+        new ProductDAO().setSaleAndReviewNumber(c.getProducts());
+
+        String sort = request.getParameter("sort");
+        if (null!=sort) {
+            switch (sort) {
+                case "review":
+                    Collections.sort(c.getProducts(),new ProductReviewComparator());
+                    break;
+                case "date":
+                    Collections.sort(c.getProducts(), new ProductDateComparator());
+                    break;
+                case "saleCount":
+                    Collections.sort(c.getProducts(), new ProductSaleCountComparator());
+                    break;
+                case "price":
+                    Collections.sort(c.getProducts(), new ProductPriceComparator());
+                    break;
+                case "all":
+                    Collections.sort(c.getProducts(), new ProductAllComparator());
+                    break;
+            }
+        }
+        request.setAttribute("c", c);
+        return "category.jsp";
+    }
+
+    public String search(HttpServletRequest request, HttpServletResponse response, Page page) {
+        String keyword = request.getParameter("keyword");
+        List<Product> ps = new ProductDAO().search(keyword, 0, 20);
+        productDAO.setSaleAndReviewNumber(ps);
+        request.setAttribute("ps", ps);
+        return "searchResult.jsp";
+    }
+
+    public String buyOne(HttpServletRequest request, HttpServletResponse response, Page page) {
+        int pid = Integer.parseInt(request.getParameter("pid"));
+        int num = Integer.parseInt(request.getParameter("num"));
+        Product product = productDAO.get(pid);
+        int oiid = 0;
+
+        User user = (User) request.getSession().getAttribute("user");
+        boolean found = false;
+        List<OrderItem> ois = orderItemDAO.listByUser(user.getId());
+        for (OrderItem oi : ois) {
+            if (oi.getProduct().getId()== product.getId()) {
+                oi.setNumber(oi.getNumber()+num);
+                orderItemDAO.update(oi);
+                found = true;
+                oiid = oi.getId();
+                break;;
+            }
+        }
+        if (!found) {
+            OrderItem oi = new OrderItem();
+            oi.setUser(user);
+            oi.setNumber(num);
+            oi.setProduct(product);
+            orderItemDAO.add(oi);
+            oiid = oi.getId();
+        }
+        return "@forebuy?oiid="+oiid;
+    }
+
+    public String buy(HttpServletRequest request, HttpServletResponse response, Page page) {
+        String[] oiids = request.getParameterValues("oiid");
+        List<OrderItem> ois = new ArrayList<>();
+        float total = 0;
+
     }
 }
